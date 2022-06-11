2d3605900fc463f32d96d975552d1af5417709c2
Maciej Derulski <macder7@gmail.com>
2022-06-10 21:25:10 -0400

    create shippo order base

--- src/services/shippo-fulfillment.js
+++ src/services/shippo-fulfillment.js
@@ -1,23 +1,32 @@
 import { FulfillmentService } from "medusa-interfaces"
-import { MedusaError } from "medusa-core-utils"
+import { humanizeAmount, MedusaError } from "medusa-core-utils"
 import shippo from "shippo"
 
 // TODO: move to medusa-config plugins array
-const SHIPPO_API_KEY = process.env.SHIPPO_API_KEY
+const SHIPPO_API_KEY = process.env.SHIPPO_API_KEY 
+const WEIGHT_UNIT_TYPE = 'g'
+const DIMENSION_UNIT_TYPE = 'cm'
+
 class ShippoFulfillmentService extends FulfillmentService {
   static identifier = 'shippo'
 
-  constructor({ cartService, addressRepository }, options) {
+  constructor({
+    addressRepository,
+    cartService,
+    totalsService }, options) {
     super()
-
-    /** @private @const {CartService} */
-    this.cartService_ = cartService
 
     /** @private @const {AddressRepository} */
     this.addressRepository_ = addressRepository
 
+    /** @private @const {CartService} */
+    this.cartService_ = cartService
+
     /** @private @const {Shippo} */
     this.shippo_ = shippo(SHIPPO_API_KEY)
+
+    /** @private @const {TotalsService} */
+    this.totalsService_ = totalsService
   }
 
   async getFulfillmentOptions() {
@@ -58,8 +67,10 @@ class ShippoFulfillmentService extends FulfillmentService {
 
     const shippoAddressId = await this.createShippoAddress(shippingAddress, cart.email)
       .then(r => {
-        if (!r.validation_results.is_valid) {
-          throw r.validation_results.messages[0]
+        if (r.country == 'US') { // shippo address validation is free for only USA addresses
+          if (!r.validation_results.is_valid) {
+            throw r.validation_results.messages[0]
+          }
         }
         return r.object_id
       }).catch(e => {
@@ -78,6 +89,53 @@ class ShippoFulfillmentService extends FulfillmentService {
     fromOrder,
     fulfillment
   ) {
+    const toAddress = await this.createShippoAddress(fromOrder.shipping_address, fromOrder.email)
+    
+    const lineItems = await Promise.all(
+      fulfillmentItems.map(async item => {
+        const totals = await this.totalsService_.getLineItemTotals(
+          item,
+          fromOrder,
+          {
+            include_tax: true,
+            use_tax_lines: true,
+          }
+        )
+
+        return {
+          quantity: item.quantity,
+          sku: item.variant.sku,
+          title: item.variant.product.title,
+          total_price:
+            humanizeAmount(
+              totals.subtotal,
+              fromOrder.currency_code
+            ),
+          currency: fromOrder.currency_code,
+          weight: item.variant.weight,
+          weight_unit: WEIGHT_UNIT_TYPE
+        }
+      })
+    )
+
+    const totalWeight = lineItems
+      .map(e => e.weight * e.quantity)
+      .reduce((sum, current) => sum + current, 0)
+
+    const shippoOrder = await this.shippo_.order.create({
+      to_address: toAddress.object_id,
+      line_items: lineItems,
+      placed_at: fromOrder.created_at,
+      shipping_cost: humanizeAmount(
+        fromOrder.shipping_methods[0].price,
+        fromOrder.currency_code
+      ),
+      shipping_cost_currency: fromOrder.currency_code,
+      shipping_method: fromOrder.shipping_methods[0].shipping_option.name,
+      weight: totalWeight,
+      weight_unit: WEIGHT_UNIT_TYPE
+    })
+
   }
 
   canCalculate(data) {
@@ -101,7 +159,7 @@ class ShippoFulfillmentService extends FulfillmentService {
       "country": address.country_code, // iso2 country code
       "phone": address.phone,
       "email": email,
-      "validate": true,
+      "validate": (address.country_code == 'us') ?? true,
     })
   }
 }
