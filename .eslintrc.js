bfa15721a32df718e3b2d545325d73d693095336
Maciej Derulski <macder7@gmail.com>
2022-06-18 16:59:44 -0400

    config eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  parser: `@babel/eslint-parser`,
+  parserOptions: {
+    requireConfigFile: false,
+    ecmaFeatures: {
+      experimentalDecorators: true,
+    },
+  },
+  plugins: [`eslint-plugin-prettier`],
+  extends: [`eslint:recommended`, `google`, `eslint-config-prettier`],
+  rules: {
+    "prettier/prettier": `error`,
+    curly: [2, `all`],
+    "new-cap": `off`,
+    "require-jsdoc": `off`,
+    semi: `off`,
+    "no-unused-expressions": `off`,
+    camelcase: `off`,
+    "no-invalid-this": `off`,
+  },
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+}
