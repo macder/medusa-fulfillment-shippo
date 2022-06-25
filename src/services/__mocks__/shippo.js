5f8e34aeb32ac96fd4ec062ffa3c35fec7be9703
Maciej Derulski <macder7@gmail.com>
2022-06-25 04:22:42 -0400

    Auto stash before revert of "add initial tests for shippo-client service class"

--- src/services/__mocks__/shippo.js
+++ src/services/__mocks__/shippo.js
@@ -2,11 +2,15 @@ import data from "../__mocks__/shippo-data.json"
 
 const shippo = () => ({
   carrieraccount: {
-    list: jest.fn(async () => ({ results: [...data.carrier_accounts] })),
+    list: jest.fn(
+      async () => ({ results: [...data.carrier_accounts] })
+    )
   },
   servicegroups: {
-    list: jest.fn(async () => [...data.service_groups]),
-  },
+    list: jest.fn(
+      async () => ([ ...data.service_groups ])
+    )
+  }
 })
 
 export default shippo
--- src/services/__tests__/shippo-client.js
+++ src/services/__tests__/shippo-client.js
@@ -4,38 +4,42 @@ import data from "../__mocks__/shippo-data.json"
 
 describe("ShippoFulfillmentService", () => {
   describe("findActiveCarriers", () => {
-    const shippoClientService = new ShippoClientService({}, {})
+    const shippoClientService = new ShippoClientService(
+      {}, {}
+    )
 
     beforeAll(async () => {
       jest.clearAllMocks()
     })
 
     it("returns only active carriers accounts", async () => {
-      const findActiveCarriers = await shippoClientService.findActiveCarriers_([
-        ...data.carrier_accounts,
-      ])
+      const findActiveCarriers = await shippoClientService
+        .findActiveCarriers_([...data.carrier_accounts])
 
-      findActiveCarriers.forEach((item) => expect(item.active).toBe(true))
+      findActiveCarriers.forEach(
+        item => expect(item.active).toBe(true)
+      )
     })
   })
 
   describe("splitCarriersToServices_", () => {
-    const shippoClientService = new ShippoClientService({}, {})
+    const shippoClientService = new ShippoClientService(
+      {}, {}
+    )
 
     beforeAll(async () => {
       jest.clearAllMocks()
     })
 
     it("splits carrier objects into service levels", async () => {
-      const carriers = data.carrier_accounts.filter(
-        (item) => item.active === true
-      )
-
-      const serviceOptions = await shippoClientService.splitCarriersToServices_(
-        carriers
-      )
-
+      
+      const carriers = data.carrier_accounts.filter(item => item.active === true)
+      
+      const serviceOptions = await shippoClientService
+        .splitCarriersToServices_(carriers)
+        
       expect(serviceOptions).toStrictEqual(data.service_level_option)
     })
   })
+
 })
