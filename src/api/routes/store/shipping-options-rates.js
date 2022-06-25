04435fb2d2ee15f024a85643e3432c4bf4602095
Maciej Derulski <macder7@gmail.com>
2022-06-25 19:30:27 -0400

    simplify

--- src/api/routes/store/shipping-options-rates.js
+++ src/api/routes/store/shipping-options-rates.js
@@ -2,8 +2,7 @@ export default async (req, res, next) => {
   const { cart_id } = req.params
   const shippoFulfillmentService = req.scope.resolve("shippoFulfillmentService")
 
-  const customShippingOptions =
+  res.json(
     await shippoFulfillmentService.updateShippingRates(cart_id)
-
-  res.json({ customShippingOptions })
+  )
 }
