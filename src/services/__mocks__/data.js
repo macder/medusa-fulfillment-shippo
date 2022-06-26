83f66d59493be07fa5a49c3869946fdfc5e18fd9
Maciej Derulski <macder7@gmail.com>
2022-06-26 03:12:04 -0400

    address mock

--- src/services/__mocks__/data.js
+++ src/services/__mocks__/data.js
@@ -1,5 +1,24 @@
 import { faker } from "@faker-js/faker"
 
+export const mockAddress = () => ({
+  id: faker.database.mongodbObjectId(),
+  created_at: faker.date.past(),
+  updated_at: faker.date.past(),
+  deleted_at: null,
+  customer_id: faker.database.mongodbObjectId(),
+  company: faker.company.companyName(),
+  first_name: faker.name.firstName(),
+  last_name: faker.name.lastName(),
+  address_1: faker.address.streetAddress(),
+  address_2: faker.address.secondaryAddress(),
+  city: faker.address.cityName(),
+  country_code: faker.address.countryCode().toLowerCase(),
+  province: faker.address.stateAbbr(),
+  postal_code: faker.address.zipCode(),
+  phone: faker.phone.phoneNumber(),
+  metadata: null
+})
+
 export const mockParcelTemplate = () => ({
   object_id: faker.database.mongodbObjectId(),
   object_owner: faker.internet.email(),
