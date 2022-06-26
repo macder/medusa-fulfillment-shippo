40abdc56581c7ce1a4e1431c9bc062560af2553d
Maciej Derulski <macder7@gmail.com>
2022-06-26 05:54:16 -0400

    WIP - data mocks

--- src/services/__mocks__/data.js
+++ src/services/__mocks__/data.js
@@ -4,7 +4,7 @@ export const makeArrayOf = (mockFn, count) =>
   [...Array(count).keys()].map((e) => mockFn())
 
 export const mockAddress = () => ({
-  id: faker.database.mongodbObjectId(),
+  id: `addr_${faker.database.mongodbObjectId()}`,
   created_at: faker.date.past(),
   updated_at: faker.date.past(),
   deleted_at: null,
@@ -35,3 +35,111 @@ export const mockParcelTemplate = () => ({
   weight: faker.datatype.number({ min: 200, max: 3000 }),
   weight_unit: "g",
 })
+
+// WIP
+export const mockProduct = () => ({
+  id: `prod_${faker.database.mongodbObjectId()}`,
+  created_at: faker.date.past(),
+  updated_at: faker.date.past(),
+  deleted_at: null,
+  title: faker.commerce.productName(),
+  subtitle: null,
+  description: faker.commerce.productDescription(),
+  handle: "coffee-mug",
+  is_giftcard: false,
+  status: "published",
+  thumbnail: "",
+  profile_id: faker.database.mongodbObjectId(),
+  weight: faker.datatype.number({ min: 200, max: 3000 }),
+  length: faker.datatype.number({ min: 20, max: 100 }),
+  width: faker.datatype.number({ min: 20, max: 100 }),
+  height: faker.datatype.number({ min: 20, max: 100 }),
+  hs_code: "HS_CODE_555",
+  origin_country: "India",
+  mid_code: "555MID_CODE_TEST",
+  material: faker.commerce.productMaterial(),
+  collection_id: null,
+  type_id: null,
+  discountable: true,
+  external_id: null,
+  metadata: null,
+})
+
+// WIP
+export const mockVariant = () => ({
+  id: `variant_${faker.database.mongodbObjectId()}`,
+  created_at: faker.date.past(),
+  updated_at: faker.date.past(),
+  deleted_at: null,
+  title: "One Size",
+  product_id: `prod_${faker.database.mongodbObjectId()}`,
+  sku: "SKU_935611",
+  barcode: "123456789012",
+  ean: "1234567890123",
+  upc: null,
+  inventory_quantity: 149,
+  allow_backorder: false,
+  manage_inventory: true,
+  hs_code: "HS_CODE_4321",
+  origin_country: "India",
+  mid_code: "MID_CODE_858",
+  material: faker.commerce.productMaterial(),
+  weight: faker.datatype.number({ min: 200, max: 3000 }),
+  length: faker.datatype.number({ min: 20, max: 100 }),
+  width: faker.datatype.number({ min: 20, max: 100 }),
+  height: faker.datatype.number({ min: 20, max: 100 }),
+  metadata: null,
+  product: {}, // prod_
+})
+
+export const mockItem = () => ({
+  id: `item_${faker.database.mongodbObjectId()}`,
+  created_at: "2022-06-24T18:08:31.058Z",
+  updated_at: "2022-06-24T18:10:39.925Z",
+  cart_id: `cart_${faker.database.mongodbObjectId()}`,
+  order_id: `order_${faker.database.mongodbObjectId()}`,
+  swap_id: null,
+  claim_order_id: null,
+  title: "Medusa Coffee Mug",
+  description: "One Size",
+  thumbnail: "",
+  is_return: false,
+  is_giftcard: false,
+  should_merge: true,
+  allow_discounts: true,
+  has_shipping: true,
+  unit_price: 1000,
+  variant_id: "variant_01G4XYVBYWTVR3WGWFDEGMA7CQ",
+  quantity: 2,
+  fulfilled_quantity: null,
+  returned_quantity: null,
+  shipped_quantity: null,
+  metadata: {},
+  variant: {}, // variant_
+  tax_lines: [], // litl_
+  adjustments: [],
+})
+
+// WIP
+export const mockLineItem = () => {
+  const id = {
+    prod: `prod_${faker.database.mongodbObjectId()}`,
+    variant: `variant_${faker.database.mongodbObjectId()}`,
+  }
+
+  const lineItem = {
+    ...mockItem(),
+    variant: {
+      ...mockVariant(),
+      product_id: id.prod,
+      product: {
+        ...mockProduct(),
+        id: id.prod,
+      },
+    },
+  }
+
+  return lineItem
+}
+
+console.log(mockLineItem())
