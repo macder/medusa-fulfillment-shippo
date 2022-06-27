c8fa0ca477cf72c8a1778b09f580d1b7d0b6cd3a
Maciej Derulski <macder7@gmail.com>
2022-06-26 21:01:40 -0400

    data mocks - add function to snake case string

--- src/services/__mocks__/data/data-utils.js
+++ src/services/__mocks__/data/data-utils.js
@@ -6,3 +6,14 @@
  */
 export const makeArrayOfMocks = (mockFn, count, args = {}) =>
   [...Array(count).keys()].map(() => mockFn(args))
+
+/** toSnakeCase
+* @param {string} str - string to snake case
+* @return {string} - snake case string
+*/
+export const toSnakeCase = (str) =>
+  str &&
+  str
+    .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+    .map((x) => x.toLowerCase())
+    .join("_")
