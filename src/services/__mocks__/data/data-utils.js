b6408400ce98863ba29e0ef04530bba2209643f3
Maciej Derulski <macder7@gmail.com>
2022-06-26 20:13:16 -0400

    doc data util for mocks

--- src/services/__mocks__/data/data-utils.js
+++ src/services/__mocks__/data/data-utils.js
@@ -1,2 +1,8 @@
+/** makeArrayOfMocks
+ * @param {function} mockFn - the data mock function to use
+ * @param {int} count - array length
+ * @param {object} [args] - params for mockFn
+ * @return {array} - array of result objects from mockFn
+ */
 export const makeArrayOfMocks = (mockFn, count, args = {}) =>
   [...Array(count).keys()].map((e) => mockFn(args))
