fd3d32f741e5f53bed59d6b8487c2dc1664e9ec9
Maciej Derulski <macder7@gmail.com>
2022-07-04 22:44:09 -0400

    more transparent

--- src/api/routes/hooks/transactions.js
+++ src/api/routes/hooks/transactions.js
@@ -12,14 +12,14 @@ export default async (req, res, next) => {
   }
 
   if (validToken) {
-    const { data } = req.body
+    const untrustedData = req.body.data
 
-    if (data.object_state === "VALID" && data.status === "SUCCESS") {
+    if (untrustedData.object_state === "VALID" && untrustedData.status === "SUCCESS") {
       // Verify received input is a real shippo transaction object by fetching it from shippo api.
       // Then, if it exist, use it as the trusted data going forward instead of the input (req.body)... 
       // Otherwise respond with a 500 and go back to sleep...
       const transaction = await shippoClientService
-        .fetchTransaction(data.object_id)
+        .fetchTransaction(untrustedData.object_id)
         .catch((e) => console.error(e))
 
       if (transaction?.object_state === "VALID") {
