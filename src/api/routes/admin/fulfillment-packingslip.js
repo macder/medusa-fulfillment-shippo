9c5e345ef0bdec68e73887ef91d0920863f73c18
Maciej Derulski <macder7@gmail.com>
2022-07-14 00:44:56 -0400

    resolves #174, resolves #175

--- src/api/routes/admin/fulfillment-packingslip.js
+++ src/api/routes/admin/fulfillment-packingslip.js
@@ -1,16 +1,8 @@
 export default async (req, res) => {
-  const fulfillmentService = req.scope.resolve("fulfillmentService")
   const shippoClientService = req.scope.resolve("shippoClientService")
   const { fulfillment_id } = req.params
 
-  res.json(
-    await fulfillmentService
-      .retrieve(fulfillment_id)
-      .then(
-        async ({ data: { shippo_order_id } }) =>
-          await shippoClientService.useClient.order
-            .packingslip(shippo_order_id)
-            .then((response) => ({ ...response }))
-      )
-  )
+  const packingSlip = await shippoClientService.fetchPackingSlip(fulfillment_id)
+
+  res.json(packingSlip)
 }
