30b3c30320c3a73b3948f53cb25047e61bc16f71
Maciej Derulski <macder7@gmail.com>
2022-07-15 15:36:18 -0400

    updated tests

--- src/services/__tests__/shippo-client.js
+++ src/services/__tests__/shippo-client.js
@@ -1,9 +1,52 @@
 import * as matchers from "jest-extended"
 import ShippoClientService from "../shippo-client"
+import shippo from "shippo"
 
 expect.extend(matchers)
 
 describe("ShippoClientService", () => {
+  const fulfillmentService = {
+    retrieve: jest.fn(async (id) => ({
+      id: "ful_id_123",
+      data: {
+        shippo_order_id: "order_id_123",
+      },
+    })),
+  }
+
+  describe("fetchOrder", () => {
+    beforeAll(async () => {
+      jest.clearAllMocks()
+    })
+
+    const shippoClientService = new ShippoClientService(
+      { fulfillmentService },
+      {}
+    )
+
+    it("fetches shippo order with correct id", async () => {
+      const result = await shippoClientService.fetchOrder("ful_id_123")
+      expect(result).toBeObject()
+      expect(result).toContainEntry(["object_id", "order_id_123"])
+    })
+  })
+
+  describe("fetchPackingSlip", () => {
+    beforeAll(async () => {
+      jest.clearAllMocks()
+    })
+
+    const shippoClientService = new ShippoClientService(
+      { fulfillmentService },
+      {}
+    )
+
+    it("fetches packing slip", async () => {
+      const result = await shippoClientService.fetchPackingSlip("ful_id_123")
+      expect(result).toBeObject()
+    })
+  })
+
   describe("fetchUserParcelTemplates", () => {
     const shippoClientService = new ShippoClientService({}, {})
 
