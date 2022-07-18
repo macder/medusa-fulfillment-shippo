e8fb31398fec4bbee9bf29064af76d4365c8a49d
Maciej Derulski <macder@users.noreply.github.com>
2022-07-17 20:26:12 -0400

    Rest api removal (#206)
    * REST API removal #181
    
    * update readme

--- src/api/routes/hooks/index.js
+++ src/api/routes/hooks/index.js
@@ -1,31 +1,17 @@
 import { Router } from "express"
 import rateLimit from "express-rate-limit"
 import bodyParser from "body-parser"
-import cors from "cors"
-import { getConfigFile } from "medusa-core-utils"
 import middlewares from "../../middlewares"
 
 const route = Router()
 
-export default (app, rootDirectory) => {
-  const { configModule } = getConfigFile(rootDirectory, "medusa-config")
-  const config = (configModule && configModule.projectConfig) || {}
-
-  const storeCors = config.store_cors || ""
-
+export default (app) => {
   const apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
     max: 100,
     standardHeaders: true,
     legacyHeaders: false,
   })
-
-  route.use(
-    cors({
-      origin: storeCors.split(","),
-      credentials: true,
-    })
-  )
 
   app.use("/hooks", route)
   route.use(bodyParser.json())
