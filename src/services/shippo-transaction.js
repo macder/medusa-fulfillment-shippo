373caca063a20a112b0dd61f652ff343cb22ceda
Maciej Derulski <macder7@gmail.com>
2022-07-19 18:31:33 -0400

    fix and optimize transaction fetches

--- src/services/shippo-transaction.js
+++ src/services/shippo-transaction.js
@@ -2,8 +2,10 @@ import { BaseService } from "medusa-interfaces"
 
 class ShippoTransactionService extends BaseService {
   #client
+  #fetchBy
   #orderService
   #shippo
+  #transaction
 
   constructor({ orderService, shippoClientService }, options) {
     super()
@@ -25,20 +27,23 @@ class ShippoTransactionService extends BaseService {
    * @return {object} The transaction
    */
   async fetch(id) {
-    return await this.#client.transaction.retrieve(id)
+    if(this.#transaction?.object_id !== id) {
+      this.#transaction = await this.#client.transaction.retrieve(id)
+    }
+    return this.#transaction
   }
 
   /**
    * Fetch the extended version of a transaction
-   * @param {string|object} transaction - shippo transaction id or object
+   * @param {string|object} transaction - shippo transaction id
    * @return {object} The extended transaction
    */
-  async fetchExtended(transaction) {
-    const order = await this.findOrder(transaction)
+  async fetchExtended(transactionId) {
+    const order = await this.findOrder(transactionId)
     const transactions = await this.#shippo.fetchExtendedTransactions(order)
 
     return transactions.find(
-      ({ object_id }) => object_id === transaction.object_id
+      ({ object_id }) => object_id === this.#transaction.object_id
     )
   }
 
@@ -64,14 +69,14 @@ class ShippoTransactionService extends BaseService {
 
   /**
    * Finds the fulfillment associated with the transaction
-   * @param {string|object} transaction - shippo transaction id or object
+   * @param {string|object} transaction - shippo transaction
    * @return {Fulfillment} The fulfillment related to this transaction
    */
-  async findFulfillment(transaction) {
-    const order = await this.findOrder(transaction)
+  async findFulfillment(transactionId) {
+    const order = await this.findOrder(transactionId)
 
     return order.fulfillments.find(
-      ({ data: { shippo_order_id } }) => shippo_order_id === transaction.order
+      ({ data: { shippo_order_id } }) => shippo_order_id === this.#transaction.order
     )
   }
 
@@ -80,7 +85,8 @@ class ShippoTransactionService extends BaseService {
    * @param {string|object} transaction - shippo transaction id or object
    * @return {Order} The order related to this transaction
    */
-  async findOrder(transaction) {
+  async findOrder(transactionId) {
+    const transaction = await this.fetch(transactionId)
     const orderDisplayId = await this.#parseOrderDisplayId(transaction)
     return await this.#retrieveOrderByDisplayId(orderDisplayId)
   }
