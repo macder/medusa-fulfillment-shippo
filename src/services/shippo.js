0d5fd64fd9d31152fd5e48e322967e7f3cc24e6d
Maciej Derulski <macder7@gmail.com>
2022-07-19 20:40:31 -0400

    wip - public interface

--- src/services/shippo.js
+++ src/services/shippo.js
@@ -1,7 +1,7 @@
 import { BaseService } from "medusa-interfaces"
 
 class ShippoService extends BaseService {
-  #manager
+  #client
   #shippoClient
   #shippoOrder
   #shippoPacker
@@ -11,7 +11,6 @@ class ShippoService extends BaseService {
 
   constructor(
     {
-      manager,
       shippoClientService,
       shippoOrderService,
       shippoPackerService,
@@ -41,30 +40,61 @@ class ShippoService extends BaseService {
     /** @private @const {ShippoTransactionService} */
     this.#shippoTransaction = shippoTransactionService
 
-    /** @private @const {Manager} */
-    this.#manager = manager
+    this.#client = this.#shippoClient.getClient()
 
-    /** @private @const {Shippo} */
-    // this.#client = this.#shippo.useClient
-
+    this.account = this.#account()
+    this.client = this.#shippoClient.getClient()
     this.order = this.#order()
     this.packer = this.#packer()
+    this.packingslip = this.#packingslip()
     this.rates = this.#rates()
     this.track = this.#track()
     this.transaction = this.#transaction()
+
+    this.fulfillment = this.#fulfillment()
+  }
+
+  #account() {
+    return {
+      address: async () => await this.#shippoClient.fetchSenderAddress()
+    }
+  }
+
+  #fulfillment() {
+    const fetchBy = {
+      transaction: async (id) => await this.#shippoTransaction.findFulfillment(id)
+    }
+
+    return {
+      fetchBy: async ([entity, id]) => await fetchBy[entity](id)
+    }
   }
 
   #order() {
+    const fetchBy = {
+      fullfillment: async (id) => await this.#shippoOrder.fetchByFulfillmentId(id),
+    }
+
     return {
       fetch: async (id) => await this.#shippoOrder.fetch(id),
-      fetchBy: async ([entity, id]) =>
-        await this.#shippoOrder.fetchBy([entity, id]),
+      fetchBy: async ([entity, id]) => await fetchBy[entity](id)
     }
   }
 
   #packer() {
     return {
       pack: async (items) => await this.#shippoPacker.packBins(items)
+    }
+  }
+
+  #packingslip() {
+    const fetchBy = {
+      fulfillment: async (id) => await this.#shippoOrder.fetchPackingSlipByFulfillmentId(id)
+    }
+
+    return {
+      fetch: async (id) => await this.#shippoOrder.fetchPackingSlip(id),
+      fetchBy: async ([entity, id]) => await fetchBy[entity](id)   
     }
   }
 
