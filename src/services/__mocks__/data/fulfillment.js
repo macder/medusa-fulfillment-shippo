c6c0a32f31bfe4e9879b6b49532668d42ae7ee62
Maciej Derulski <macder7@gmail.com>
2022-07-30 00:17:24 -0400

    solid order mock

--- src/services/__mocks__/data/fulfillment.js
+++ src/services/__mocks__/data/fulfillment.js
@@ -1,33 +1,50 @@
 import { makeArrayOf } from "./data-utils"
 
-const fulfillmentItem = (...[fulfillment_id, item_id, quantity]) =>
+const fulfillmentItem = (props) =>
   Object.freeze({
-    fulfillment_id,
-    item_id,
-    quantity,
+    fulfillment_id: props.get("fulfillment_id"),
+    item_id: props.get("item_id"),
+    quantity: props.get("quantity"),
   })
 
-const fulfillment = (id, getId) => (getItems, getTrackingLinks) =>
+const fulfillment = (props) => (items, trackingLinks) =>
   Object.freeze({
-    id,
-    order_id: getId("order"),
-    claim_order_id: getId("claim"),
-    swap_id: getId("swap"),
+    id: props.get("id"),
+    order_id: props.get("order_id"),
+    claim_order_id: props.get("claim_id"),
+    swap_id: props.get("swap_id"),
     no_notification: false,
     provider_id: "shippo",
     tracking_numbers: [],
-    data: {
-      shippo_order_id: getId("shippo_order"),
-    },
-    shipped_at: "2022-07-27T14:55:50.745Z",
+    data: Object.freeze({
+      shippo_order_id: props.get("shippo_order_id"),
+    }),
+    shipped_at: "",
     canceled_at: null,
     metadata: {},
-    idempotency_key: null,
-    tracking_links: getTrackingLinks(),
-    items: getItems(id),
+    tracking_links: trackingLinks(),
+    items: items(props),
   })
 
-export const mockfulfillmentItem = (fulfillment_id, item_id, quantity = 1) =>
-  fulfillmentItem(fulfillment_id, item_id, quantity)
+export const fulfillmentItemMock = (props) => fulfillmentItem(props)
 
-export const mockFulfillment = ({ id, getId }) => fulfillment(id, getId)
+export const fulfillmentMock = (props) => fulfillment(props)
+
+export const fulfillmentItemArrayMock = (propMap) =>
+  makeArrayOf((i) => {
+    const props = new Map()
+    props.set("fulfillment_id", propMap.get("id"))
+    props.set("item_id", propMap.get("items")[i].id)
+    props.set("quantity", propMap.get("items")[i].quantity)
+    return fulfillmentItemMock(props)
+  }, propMap.get("items").length)
+
+export const fulfillmentArrayMock = (propMap) =>
+  makeArrayOf((i) => {
+    const props = new Map(Object.entries(propMap.get("fulfillments")[i]))
+    propMap.get("setIds")(propMap, props)
+    return fulfillmentMock(props)(
+      fulfillmentItemArrayMock,
+      () => []
+    )
+  }, propMap.get("fulfillments").length)
