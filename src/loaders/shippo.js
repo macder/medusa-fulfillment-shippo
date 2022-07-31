5b587d6dc863b9752e7f398b66aeb1495d41822d
Maciej Derulski <macder7@gmail.com>
2022-07-31 02:05:59 -0400

    use logger instead

--- src/loaders/shippo.js
+++ src/loaders/shippo.js
@@ -5,9 +5,11 @@ export default async (container) => {
   const manager = container.resolve("manager")
   const config = shippoFulfillmentService.getWebhookConfig()
 
+  const logger = container.resolve("logger")
+
   if (config.webhook_test_mode) {
-    console.warn(
-      "\x1b[33m WARNING: medusa-fulfillment-shippo - Webhook test mode enabled: Accepting untrusted input from req.body\x1b[0m"
+    logger.warn(
+      "medusa-fulfillment-shippo - Webhook test mode enabled: Accepting untrusted input from req.body"
     )
   }
 
