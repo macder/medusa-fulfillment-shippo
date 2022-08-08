53140fbf2e1a6610d0bc53da49612dcd42305afb
Maciej Derulski <macder7@gmail.com>
2022-08-08 18:40:36 -0400

    add user-parcel mock

--- src/services/__mocks__/shippo-client.js
+++ src/services/__mocks__/shippo-client.js
@@ -3,56 +3,52 @@ import {
   shippoTransactionMock,
   shippoTransactionExtendedMock,
 } from "./shippo/transaction"
+import { userParcelMock } from "./shippo/user-parcel"
 
-export const shippoClientMock = ({ ...state }) => {
-  const order = shippoOrderMock(state.order)
-  const transaction = shippoTransactionMock(state?.transaction?.label)
+export const shippoClientMock = ({ ...state }) => ({
+  account: {
+    address: jest.fn(async () => ({
+      results: [
+        {
+          is_default_sender: true,
+          is_default_return: true,
+        },
+        {
+          is_default_sender: false,
+          is_default_return: false,
+        },
+      ],
+    })),
+  },
+  order: {
+    retrieve: jest.fn(async (object_id) =>
+      shippoOrderMock(state.order)(object_id)
+    ),
 
-  return {
-    account: {
-      address: jest.fn(async () => ({
-        results: [
-          {
-            is_default_sender: true,
-            is_default_return: true,
-          },
-          {
-            is_default_sender: false,
-            is_default_return: false,
-          },
-        ],
-      })),
-    },
-    order: {
-      retrieve: jest.fn(async (object_id) => order(object_id)),
+    packingslip: jest.fn(async () => ({
+      expires: "",
+      slip_url: "https://shippo-delivery.net",
+      created: "",
+    })),
+  },
+  transaction: {
+    retrieve: jest.fn(async (object_id) =>
+      shippoTransactionMock(state?.transaction?.label)(object_id)
+    ),
 
-      packingslip: jest.fn(async () => ({
-        expires: "",
-        slip_url: "https://shippo-delivery.net",
-        created: "",
-      })),
-    },
-    transaction: {
-      retrieve: jest.fn(async (object_id) => transaction(object_id)),
-
-      search: jest.fn(async (q) =>
-        // const id = q.replace(/[^0-9]/g, "")
-        // const transactions = config(({ transactions }) => transactions)
-        ({
-          results: state.order.transactions.map((ta) =>
-            shippoTransactionExtendedMock(
-              ta.object_id === "ta_label"
-                ? state.transaction.label
-                : state.transaction.return
-            )(ta.object_id)
-          ),
-        })
+    search: jest.fn(async (q) => ({
+      results: state.order.transactions.map((ta) =>
+        shippoTransactionExtendedMock(
+          ta.object_id === "ta_label"
+            ? state.transaction.label
+            : state.transaction.return
+        )(ta.object_id)
       ),
-    },
-    // userparceltemplates: {
-    //   list: jest.fn(async () => ({
-    //     results: userParcelProps().map((props) => userParcelTemplate(props)),
-    //   })),
-    // },
-  }
-}
+    })),
+  },
+  userparceltemplates: {
+    list: jest.fn(async () => ({
+      results: state.user_parcels.map((parcel) => userParcelMock(parcel)),
+    })),
+  },
+})
---
+++ src/services/__mocks__/shippo/user-parcel/state.js
@@ -0,0 +1,23 @@
+export const userParcelState = () => [
+  {
+    object_id: "upt_1",
+    name: "Small",
+    length: "15",
+    width: "15",
+    height: "10",
+  },
+  {
+    object_id: "upt_2",
+    name: "Medium",
+    length: "30",
+    width: "30",
+    height: "20",
+  },
+  {
+    object_id: "upt_3",
+    name: "Large",
+    length: "60",
+    width: "60",
+    height: "40",
+  },
+]
