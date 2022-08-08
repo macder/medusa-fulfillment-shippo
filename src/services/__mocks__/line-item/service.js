fb4b6c337041ab8c825edf895fa776915f8fa536
Maciej Derulski <macder7@gmail.com>
2022-08-08 18:40:52 -0400

    clean up

--- src/services/__mocks__/line-item/service.js
+++ src/services/__mocks__/line-item/service.js
@@ -1,11 +1,8 @@
 import { lineItemMock } from "./mock"
 
-export const lineItemServiceMock = (state) => {
-  const lineItem = (id) =>
+export const lineItemServiceMock = ({ ...state }) => ({
+  list: jest.fn(),
+  retrieve: jest.fn(async (id) =>
     lineItemMock(state)({ id: "product_id" })({ id: "variant_id" })(id)
-
-  return {
-    list: jest.fn(),
-    retrieve: jest.fn(async (id) => lineItem(id)),
-  }
-}
+  ),
+})
