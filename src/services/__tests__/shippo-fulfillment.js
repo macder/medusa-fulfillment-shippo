3bc21f60f877440cc9bd606d67a3d8ed8a176696
Maciej Derulski <macder7@gmail.com>
2022-08-09 23:20:41 -0400

    wip - fulfillment service tests

---
+++ src/services/__tests__/shippo-fulfillment.js
@@ -0,0 +1,142 @@
+import { toBeNumber, toContainKey } from "jest-extended"
+import { cartMock, cartState } from "../__mocks__/cart"
+
+import { makeShippoFulfillmentService } from "./setup"
+import { shippoClientMock } from "../__mocks__"
+import { userParcelState } from "../__mocks__/shippo/user-parcel"
+import { liveRateState } from "../__mocks__/shippo/live-rate"
+
+import {
+  fulfillmentOptionGroupSchema,
+  fulfillmentOptionServiceSchema,
+} from "../__mocks__/fulfillment-option"
+
+expect.extend({ toBeNumber, toContainKey })
+
+const mockShippoClient = shippoClientMock({
+  live_rate: liveRateState(),
+  user_parcels: userParcelState(),
+})
+
+jest.mock("@macder/shippo", () => () => mockShippoClient)
+
+describe("ShippoFulfillmentService", () => {
+  beforeAll(async () => {
+    jest.clearAllMocks()
+  })
+
+  const fulfillmentOption = (type) =>
+    ({
+      group: fulfillmentOptionGroupSchema(),
+      service: fulfillmentOptionServiceSchema(),
+    }[type])
+
+  describe("calculatePrice", () => {
+    describe("cart is ready", () => {
+      const shippoFulfillmentService = makeShippoFulfillmentService({
+        ...cartState().has.items_address_email,
+      })
+      const cart = cartMock(cartState().has.items_address_email)(
+        "cart_default_id"
+      )
+
+      test("returns number", async () => {
+        const result = await shippoFulfillmentService.calculatePrice(
+          fulfillmentOption("group"),
+          {},
+          cart
+        )
+        expect(result).toBeNumber()
+      })
+    })
+
+    describe("cart not ready", () => {
+      const shippoFulfillmentService = makeShippoFulfillmentService({
+        ...cartState().has.items,
+      })
+      const cart = cartMock(cartState().has.items)("cart_default_id")
+
+      test("returns null", async () => {
+        const result = await shippoFulfillmentService.calculatePrice(
+          fulfillmentOption("group"),
+          {},
+          cart
+        )
+        expect(result).toBeNull()
+      })
+    })
+  })
+
+  describe("canCalculate", () => {
+    const shippoFulfillmentService = makeShippoFulfillmentService({})
+
+    describe("live-rate option", () => {
+      test("returns true", async () => {
+        const result = await shippoFulfillmentService.canCalculate(
+          fulfillmentOption("group")
+        )
+        expect(result).toBe(true)
+      })
+    })
+
+    describe("service option", () => {
+      test("returns false", async () => {
+        const result = await shippoFulfillmentService.canCalculate(
+          fulfillmentOption("service")
+        )
+        expect(result).toBe(false)
+      })
+    })
+  })
+
+  describe("cancelFulfillment", () => {
+    const shippoFulfillmentService = makeShippoFulfillmentService({})
+
+    test("", async () => {
+      const result = await shippoFulfillmentService.cancelFulfillment()
+      expect(result).toStrictEqual({})
+    })
+  })
+
+  describe("createFulfillment", () => {
+    test("", async () => {
+      // const result = await shippoFulfillmentService
+      // expect(result)
+    })
+  })
+
+  describe("createReturn", () => {
+    test("", async () => {
+      // const result = await shippoFulfillmentService
+      // expect(result)
+    })
+  })
+
+  describe("getFulfillmentOptions", () => {
+    test("", async () => {
+      // const result = await shippoFulfillmentService
+      // expect(result)
+    })
+  })
+
+  describe("validateFulfillmentData", () => {
+    test("", async () => {
+      // const result = await shippoFulfillmentService
+      // expect(result)
+    })
+  })
+
+  describe("validateOption", () => {
+    test("", async () => {
+      // const result = await shippoFulfillmentService
+      // expect(result)
+    })
+  })
+
+  describe("verifyHookSecret", () => {
+    test("", async () => {
+      // const result = await shippoFulfillmentService
+      // expect(result)
+    })
+  })
+})
