06f8943cdd9d91a11d8ea6e86157e95886163acf
Maciej Derulski <macder7@gmail.com>
2022-08-09 23:20:18 -0400

    lint

--- src/loaders/shippo.js
+++ src/loaders/shippo.js
@@ -1,9 +1,7 @@
 export default async (container) => {
   const shippoFulfillmentService = container.resolve("shippoFulfillmentService")
-
   const logger = container.resolve("logger")
   const config = shippoFulfillmentService.getWebhookConfig()
-
   if (config.webhook_test_mode) {
     logger.warn(
       "medusa-fulfillment-shippo - Webhook test mode enabled: Accepting untrusted input from req.body"
