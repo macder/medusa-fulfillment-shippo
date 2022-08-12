d358ca98eb76067d9c4b11908624debbef2efdd5
Maciej Derulski <macder7@gmail.com>
2022-08-12 19:11:51 -0400

    wip

--- src/services/__tests__/shippo-service/order.js
+++ src/services/__tests__/shippo-service/order.js
@@ -1,5 +1,6 @@
 import {
   toBeArray,
+  toBeArrayOfSize,
   toBeFalse,
   toContainKey,
   toContainKeys,
@@ -17,6 +18,7 @@ import { shippoTransactionState } from "../../__mocks__/shippo/transaction"
 
 expect.extend({
   toBeArray,
+  toBeArrayOfSize,
   toBeFalse,
   toContainKey,
   toContainKeys,
@@ -90,14 +92,14 @@ describe("shippoService", () => {
             // act
             const result = shippoService.order.fetchBy(["fulfillment", id])
 
-            // // assert
+            // assert
             expect(result).rejects.toContainKeys(["type", "code", "message"])
           })
         })
       })
 
       describe("local_order", () => {
-        describe("has fulfillment", () => {
+        describe("has single fulfillment", () => {
           describe("with shippo order", () => {
             // arrange
             const shippoService = makeShippoService({
@@ -106,107 +108,151 @@ describe("shippoService", () => {
             })
             const id = "order_id"
 
-            test("returns array", async () => {
+            test("returns array with 1 member", async () => {
               // act
               const result = await shippoService.order.fetchBy([
                 "local_order",
                 id,
               ])
               // assert
-              expect(result).toBeArray()
+              expect(result).toBeArrayOfSize(1)
+            })
+
+            test("return has prop/value pair for fulfillment_id", async () => {
+              // act
+              const result = await shippoService.order.fetchBy([
+                "local_order",
+                id,
+              ])
+              // assert
+              expect(result[0]).toContainEntry([
+                "fulfillment_id",
+                "ful_has_shippo_order",
+              ])
             })
           })
 
-          describe("without shippo order", () => {})
-        })
+          describe("without shippo order", () => {
+            // arrange
+            const shippoService = makeShippoService({
+              line_items: [],
+              fulfillments: [fulfillmentState("no_shippo_order")],
+            })
+            const id = "order_id"
 
-        test("returns", async () => {
-          // arrange
-          // const shippoService = makeShippoService({
-          //   ...orderState("default")({ display_id: "11" }),
-          //   fulfillments: [fulfillmentState("ful_has_transaction_for_label")],
-          // })
-          // const id = "order_default"
-          // // act
-          // const result = await shippoService.order.fetchBy(["local_order", id])
-          // // assert
-          // expect(result).toBeArray()
-          // expect(result[0]).toContainEntry([
-          //   "fulfillment_id",
-          //   "ful_has_transaction_for_label",
-          // ])
-        })
-      })
+            test("returns promise.reject with medusa error", async () => {
+              // act
+              const result = shippoService.order.fetchBy(["local_order", id])
+              // assert
+              expect(result).rejects.toContainKeys(["type", "code", "message"])
+            })
+          })
 
-      describe("claim", () => {
-        test("returns", async () => {
-          // const shippoService = makeShippoService({
-          //   order_id: "order_default",
-          //   display_id: "11",
-          //   cart_id: "cart_default_id",
-          //   claim_order_id: null,
-          //   swap_id: null,
-          //   fulfillments: [
-          //     fulfillmentState("ful_has_transaction_for_label")],
-          // })
-          // const result = await shippoService.order.fetchBy(["claim", id])
-          // expect(result).toBeArray()
-          // expect(result[0]).toContainEntry([
-          //   "fulfillment_id",
-          //   "ful_default_id_1",
-          // ])
+          describe("with multiple shippo orders", () => {
+            // test("returns array", async () => {
+            //   // arrange
+            //   const shippoService = makeShippoService({
+            //     line_items: [],
+            //     fulfillments: [fulfillmentState("has_shippo_order")],
+            //   })
+            //   const id = "order_id"
+            //   // act
+            //   const result = await shippoService.order.fetchBy([
+            //     "local_order",
+            //     id,
+            //   ])
+            //   // assert
+            //   expect(result).toBeArray()
+          })
         })
       })
 
-      describe("swap", () => {
-        test("returns", async () => {
-          const id = "swap_01234567890"
-          // const result = await shippoService.order.fetchBy(["swap", id])
-          // expect(result).toBeArray()
-          // expect(result[0]).toContainEntry([
-          //   "fulfillment_id",
+      test("returns", async () => {
+        // arrange
+        // const shippoService = makeShippoService({
+        //   ...orderState("default")({ display_id: "11" }),
+        //   fulfillments: [fulfillmentState("ful_has_transaction_for_label")],
+        // })
+        // const id = "order_default"
+        // // act
+        // const result = await shippoService.order.fetchBy(["local_order", id])
+        // // assert
+        // expect(result).toBeArray()
+        // expect(result[0]).toContainEntry([
+        //   "fulfillment_id",
+        //   "ful_has_transaction_for_label",
+        // ])
+      })
+    })
+
+    describe("claim", () => {
+      test("returns", async () => {
+        // const shippoService = makeShippoService({
+        //   order_id: "order_default",
+        //   display_id: "11",
+        //   cart_id: "cart_default_id",
+        //   claim_order_id: null,
+        //   swap_id: null,
+        //   fulfillments: [
+        //     fulfillmentState("ful_has_transaction_for_label")],
+        // })
+        // const result = await shippoService.order.fetchBy(["claim", id])
+        // expect(result).toBeArray()
+        // expect(result[0]).toContainEntry([
+        //   "fulfillment_id",
+        //   "ful_default_id_1",
+        // ])
+      })
+    })
+
+    describe("swap", () => {
+      test("returns", async () => {
+        const id = "swap_01234567890"
+        // const result = await shippoService.order.fetchBy(["swap", id])
+        // expect(result).toBeArray()
+        // expect(result[0]).toContainEntry([
+        //   "fulfillment_id",
+        //   "ful_default_id_1",
+        // ])
+      })
+    })
+  })
+
+  describe("with", () => {
+    describe("fulfillment", () => {
+      describe("fetch", () => {
+        test("returns with fulfillment", async () => {
+          const id = "shippo_order_default_id_1"
+          // const result = await shippoService.order
+          //   .with(["fulfillment"])
+          //   .fetch(id)
+          // expect(result).toContainKey("object_id")
+          // expect(result.fulfillment).toContainEntry([
+          //   "id",
           //   "ful_default_id_1",
           // ])
         })
       })
     })
+  })
 
-    describe("with", () => {
-      describe("fulfillment", () => {
-        describe("fetch", () => {
-          test("returns with fulfillment", async () => {
-            const id = "shippo_order_default_id_1"
-            // const result = await shippoService.order
-            //   .with(["fulfillment"])
-            //   .fetch(id)
-            // expect(result).toContainKey("object_id")
-            // expect(result.fulfillment).toContainEntry([
-            //   "id",
-            //   "ful_default_id_1",
-            // ])
-          })
+  describe("is", () => {
+    describe("type", () => {
+      describe("replace", () => {
+        it("returns false", async () => {
+          const id = "shippo_order_default_id_1"
+          // const result = await shippoService
+          //   .is(["order", id], "replace")
+          //   .fetch()
+          // expect(result).toBeFalse()
         })
-      })
-    })
-
-    describe("is", () => {
-      describe("type", () => {
-        describe("replace", () => {
-          it("returns false", async () => {
-            const id = "shippo_order_default_id_1"
-            // const result = await shippoService
-            //   .is(["order", id], "replace")
-            //   .fetch()
-            // expect(result).toBeFalse()
-          })
-          // it("returns true", async () => {
-          //   const id = "shippo_order_09876543210"
-          //   const result = await shippoService
-          //     .is(["order", id], "replace")
-          //     .fetch()
-          //   expect(result).toBeTrue()
-          // })
-        })
+        // it("returns true", async () => {
+        //   const id = "shippo_order_09876543210"
+        //   const result = await shippoService
+        //     .is(["order", id], "replace")
+        //     .fetch()
+        //   expect(result).toBeTrue()
+        // })
       })
     })
   })
