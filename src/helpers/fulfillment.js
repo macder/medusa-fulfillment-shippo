8fb954e509f98d4eb65dfb6be75668020eb8e44c
Maciej Derulski <macder7@gmail.com>
2022-08-18 04:07:04 -0400

    remove error handling from helper

--- src/helpers/fulfillment.js
+++ src/helpers/fulfillment.js
@@ -1,16 +1,5 @@
-import { MedusaError } from "medusa-core-utils"
-
 const retrieveShippoId = (fulfillmentService) => async (id) => {
   const fulfillment = await fulfillmentService.retrieve(id)
-
-  if (!fulfillment.data?.shippo_order_id) {
-    return Promise.reject(
-      new MedusaError(
-        MedusaError.Types.NOT_FOUND,
-        `Shippo order not found for fulfillment with id: ${fulfillment.id}`
-      )
-    )
-  }
 
   const {
     data: { shippo_order_id },
