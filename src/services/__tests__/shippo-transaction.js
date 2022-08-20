47b209728d309f05d985bad7db7d6b93c0afcd4a
Maciej Derulski <macder7@gmail.com>
2022-08-20 03:05:22 -0400

    updated

--- src/services/__tests__/shippo-transaction.js
+++ src/services/__tests__/shippo-transaction.js
@@ -4,10 +4,13 @@ import {
   toContainKey,
   toContainKeys,
 } from "jest-extended"
-import { orderState } from "../__mocks__/order"
 import { shippoOrderState } from "../__mocks__/shippo/order"
 import { shippoTransactionState } from "../__mocks__/shippo/transaction"
-import { makeShippoService, makeShippoTransactionService } from "./setup"
+import {
+  makeShippoHelper,
+  makeShippoService,
+  makeShippoTransactionService,
+} from "./setup"
 import { shippoClientMock } from "../__mocks__"
 import { fulfillmentState } from "../__mocks__/fulfillment"
 import { lineItemState } from "../__mocks__/line-item"
@@ -22,9 +25,8 @@ const mockShippoClient = shippoClientMock({
 jest.mock("@macder/shippo", () => () => mockShippoClient)
 
 describe("ShippoTransactionService", () => {
-  beforeAll(async () => {
-    jest.clearAllMocks()
-  })
+  let shippoTransactionService
+
   const defaultIds = () => ({
     order_id: "order_default",
     display_id: "11",
@@ -33,17 +35,20 @@ describe("ShippoTransactionService", () => {
     swap_id: null,
   })
 
-  // arrange
-  const shippoTransactionService = makeShippoTransactionService({
-    ...defaultIds(),
-    line_items: [
-      lineItemState({ quantity: 1 }),
-      lineItemState({ quantity: 2 }),
-    ],
-    fulfillments: [fulfillmentState("has_transaction_for_label")],
-  })
-
   describe("pollExtended", () => {
+    beforeEach(() => {
+      const state = {
+        ...defaultIds(),
+        line_items: [
+          lineItemState({ quantity: 1 }),
+          lineItemState({ quantity: 2 }),
+        ],
+        fulfillments: [fulfillmentState("has_transaction_for_label")],
+      }
+      shippoTransactionService = makeShippoTransactionService(state)
+      makeShippoHelper(state)
+    })
+
     test("returns extended transaction", async () => {
       // act
       const result = await shippoTransactionService.pollExtended(
@@ -56,6 +61,19 @@ describe("ShippoTransactionService", () => {
   })
 
   describe("findFulfillment", () => {
+    beforeEach(() => {
+      const state = {
+        ...defaultIds(),
+        line_items: [
+          lineItemState({ quantity: 1 }),
+          lineItemState({ quantity: 2 }),
+        ],
+        fulfillments: [fulfillmentState("has_transaction_for_label")],
+      }
+      shippoTransactionService = makeShippoTransactionService(state)
+      makeShippoHelper(state)
+    })
+
     test("returns fulfillment", async () => {
       // act
       const result = await shippoTransactionService.findFulfillment(
