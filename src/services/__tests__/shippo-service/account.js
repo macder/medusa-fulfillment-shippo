950e2046d129e7b52e34cb79062c7d48aefbc5fb
Maciej Derulski <macder7@gmail.com>
2022-08-20 03:05:53 -0400

    updates

--- src/services/__tests__/shippo-service/account.js
+++ src/services/__tests__/shippo-service/account.js
@@ -9,16 +9,9 @@ const mockShippoClient = shippoClientMock({})
 jest.mock("@macder/shippo", () => () => mockShippoClient)
 
 describe("shippoService", () => {
-  beforeAll(async () => {
-    jest.clearAllMocks()
-  })
   const shippoService = makeShippoService({})
 
   describe("account", () => {
-    beforeAll(async () => {
-      jest.clearAllMocks()
-    })
-
     it("returns default address", async () => {
       const result = await shippoService.account.address()
       expect(result).toContainEntry(["is_default_sender", true])
